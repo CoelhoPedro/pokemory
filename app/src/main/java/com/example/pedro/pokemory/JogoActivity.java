6fccf7b84c6ab02e02af64b79ef699b5aed7d703
Cappelli <elizeu.cappelli@gmail.com>
2018-07-19 19:11:05 -0300

    criada funcao que seta os drawable e alterado o nome das variaveis para manter o padrao

--- app/src/main/java/com/example/pedro/pokemory/JogoActivity.java
+++ app/src/main/java/com/example/pedro/pokemory/JogoActivity.java
@@ -9,8 +9,8 @@ import java.util.Random;
 
 public class JogoActivity extends AppCompatActivity {
 
-    public ImageView Button1,Button2,Button3,Button4,Button5,Button6,Button7,Button8,
-            Button9,Button10,Button11,Button12;
+    public ImageView button1,button2,button3,button4,button5,button6,button7,button8,
+            button9,button10,button11,button12;
     public int[][] matriz;
 
     @Override
@@ -18,357 +18,118 @@ public class JogoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo);
 
-        Button1 =findViewById(R.id.ImageView1Id);
-        Button2 =findViewById(R.id.ImageView2Id);
-        Button3 =findViewById(R.id.ImageView3Id);
-        Button4 =findViewById(R.id.ImageView4Id);
-        Button5 =findViewById(R.id.ImageView5Id);
-        Button6 =findViewById(R.id.ImageView6Id);
-        Button7 =findViewById(R.id.ImageView7Id);
-        Button8 =findViewById(R.id.ImageView8Id);
-        Button9 =findViewById(R.id.ImageView9Id);
-        Button10 =findViewById(R.id.ImageView10Id);
-        Button11 =findViewById(R.id.ImageView11Id);
-        Button12 =findViewById(R.id.ImageView12Id);
+        button1 = findViewById(R.id.ImageView1Id);
+        button2 = findViewById(R.id.ImageView2Id);
+        button3 = findViewById(R.id.ImageView3Id);
+        button4 = findViewById(R.id.ImageView4Id);
+        button5 = findViewById(R.id.ImageView5Id);
+        button6 = findViewById(R.id.ImageView6Id);
+        button7 = findViewById(R.id.ImageView7Id);
+        button8 = findViewById(R.id.ImageView8Id);
+        button9 = findViewById(R.id.ImageView9Id);
+        button10 = findViewById(R.id.ImageView10Id);
+        button11 = findViewById(R.id.ImageView11Id);
+        button12 = findViewById(R.id.ImageView12Id);
 
-        Button1.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[0][0];
-
-                switch (valorImagem){
-                    case 0:
-                        Button1.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button1.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button1.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button1.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button1.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button1.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[0][0];
+                setDrawables(imageValue, button1);
 
             }
         });
 
-        Button2.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[0][1];
-
-                switch (valorImagem){
-                    case 0:
-                        Button2.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button2.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button2.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button2.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button2.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button2.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[0][1];
+                setDrawables(imageValue, button2);
 
             }
         });
 
-        Button3.setOnClickListener(new View.OnClickListener() {
+        button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[0][2];
-
-                switch (valorImagem){
-                    case 0:
-                        Button3.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button3.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button3.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button3.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button3.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button3.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[0][2];
+                setDrawables(imageValue, button3);
 
             }
         });
-        Button4.setOnClickListener(new View.OnClickListener() {
+        button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[1][0];
-
-                switch (valorImagem){
-                    case 0:
-                        Button4.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button4.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button4.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button4.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button4.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button4.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[1][0];
+                setDrawables(imageValue,button4);
 
             }
         });
-        Button5.setOnClickListener(new View.OnClickListener() {
+        button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[1][1];
-
-                switch (valorImagem){
-                    case 0:
-                        Button5.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button5.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button5.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button5.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button5.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button5.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[1][1];
+                setDrawables(imageValue, button5);
 
             }
         });
-        Button6.setOnClickListener(new View.OnClickListener() {
+        button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[1][2];
-
-                switch (valorImagem){
-                    case 0:
-                        Button6.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button6.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button6.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button6.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button6.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button6.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[1][2];
+                setDrawables(imageValue, button6);
 
             }
         });
-        Button7.setOnClickListener(new View.OnClickListener() {
+        button7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[2][0];
-
-                switch (valorImagem){
-                    case 0:
-                        Button7.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button7.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button7.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button7.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button7.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button7.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[2][0];
+                setDrawables(imageValue, button7);
 
             }
         });
-        Button8.setOnClickListener(new View.OnClickListener() {
+        button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[2][1];
-
-                switch (valorImagem){
-                    case 0:
-                        Button8.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button8.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button8.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button8.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button8.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button8.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[2][1];
+                setDrawables(imageValue, button8);
 
             }
         });
-        Button9.setOnClickListener(new View.OnClickListener() {
+        button9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[2][2];
-
-                switch (valorImagem){
-                    case 0:
-                        Button9.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button9.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button9.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button9.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button9.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button9.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[2][2];
+                setDrawables(imageValue, button9);
 
             }
         });
-        Button10.setOnClickListener(new View.OnClickListener() {
+        button10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[3][0];
-
-                switch (valorImagem){
-                    case 0:
-                        Button10.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button10.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button10.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button10.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button10.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button10.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[3][0];
+                setDrawables(imageValue, button10);
 
             }
         });
-        Button11.setOnClickListener(new View.OnClickListener() {
+        button11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[3][1];
-
-                switch (valorImagem){
-                    case 0:
-                        Button11.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button11.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button11.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button11.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button11.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button11.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[3][1];
+                setDrawables(imageValue, button11);
 
             }
         });
-        Button12.setOnClickListener(new View.OnClickListener() {
+        button12.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int valorImagem = matriz[3][2];
-
-                switch (valorImagem){
-                    case 0:
-                        Button12.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
-                        break;
-                    case 1:
-                        Button12.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
-                        break;
-                    case 2:
-                        Button12.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
-                        break;
-                    case 3:
-                        Button12.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
-                        break;
-                    case 4:
-                        Button12.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-                        break;
-                    case 5:
-                        Button12.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
-                        break;
-                }
+                int imageValue = matriz[3][2];
+                setDrawables(imageValue, button12);
 
             }
         });
+
         matriz = sorteiaPecas();
 
     }
@@ -444,6 +205,29 @@ public class JogoActivity extends AppCompatActivity {
         }
 
         return matriz;
+    }
+
+    public void setDrawables(int imgValue, ImageView button){
+        switch (imgValue){
+            case 0:
+                button.setImageDrawable(getResources().getDrawable(R.drawable.pikachu));
+                break;
+            case 1:
+                button.setImageDrawable(getResources().getDrawable(R.drawable.bulbasaur));
+                break;
+            case 2:
+                button.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
+                break;
+            case 3:
+                button.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
+                break;
+            case 4:
+                button.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
+                break;
+            case 5:
+                button.setImageDrawable(getResources().getDrawable(R.drawable.meowth));
+                break;
+        }
     }
 }
 
